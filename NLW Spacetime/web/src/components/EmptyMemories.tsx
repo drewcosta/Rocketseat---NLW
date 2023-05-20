cbd1a841afed7f3c1318b2030195e1bb89d6adbb
drewcosta <andrewcosta.contato@gmail.com>
2023-05-20 17:06:33 -0300

    quinta aula criação e listagem das memórias

--- NLW Spacetime/web/src/components/EmptyMemories.tsx
+++ NLW Spacetime/web/src/components/EmptyMemories.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const EmptyMemories = () => {
   return (
-    <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16 ">
+    <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
       <div className="flex flex-1 items-center justify-center">
         <p className="w-[360px] text-center leading-relaxed">
-          Você ainda não registrou nenhuma lembrança, comece a{''}
+          Você ainda não registrou nenhuma lembrança, comece a {''}
+          <a href="" className="underline hover:text-gray-50">
+            criar agora
+          </a>
+          !
         </p>
       </div>
-      <a href="" className="underline hover:text-gray-50">
-        criar agora
-      </a>
-      !
     </div>
   )
 }
