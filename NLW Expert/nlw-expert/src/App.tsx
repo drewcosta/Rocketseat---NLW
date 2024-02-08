f4d9858d57e94c50a249533347b5bb6feaaa45fc
Andrew Costa <andrewcosta.contato@gmail.com>
2024-02-08 19:43:22 -0300

    feat: criação do modal e das propriedades do componente note-card

--- NLW Expert/nlw-expert/src/App.tsx
+++ NLW Expert/nlw-expert/src/App.tsx
@@ -3,7 +3,6 @@ import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
 export function App() {
-
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="logo" />
@@ -21,7 +20,10 @@ export function App() {
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard />
-        <NoteCard />
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World!',
+        }} />
       </div>
     </div>
   )
