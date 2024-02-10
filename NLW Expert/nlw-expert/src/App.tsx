c434dc8412ac0195c23fcef430239415a9b3151a
Andrew Costa <andrewcosta.contato@gmail.com>
2024-02-09 21:05:50 -0300

    feat & style: Adiciona opção de deletar nota e torna modal responsivo

--- NLW Expert/nlw-expert/src/App.tsx
+++ NLW Expert/nlw-expert/src/App.tsx
@@ -12,11 +12,11 @@ interface Note {
 export function App() {
   const [search, setSearch] = useState('');
   const [notes, setNotes] = useState<Note[]>(() => {
-     const notesOnStorage = localStorage.getItem('notes');
+    const notesOnStorage = localStorage.getItem('notes');
 
-     if(notesOnStorage){
+    if (notesOnStorage) {
       return JSON.parse(notesOnStorage);
-     }
+    }
 
     return [];
   });
@@ -35,17 +35,27 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray));
   }
 
-  function handleSearchChange(event: ChangeEvent<HTMLInputElement>){
+  function onNoteDelete(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id;
+    });
+
+    setNotes(notesArray);
+
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+  }
+
+  function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
     const query = event.target.value;
     setSearch(query);
   }
 
-  const filteredNotes = search !== '' 
-  ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) 
-  : notes;
+  const filteredNotes = search !== ''
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes;
 
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={logo} alt="logo" />
 
       <form className='w-full'>
@@ -60,10 +70,10 @@ export function App() {
       {/* separator */}
       <div className='h-px bg-slate-700'></div>
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map(note => (
-          <NoteCard key={note.id} note={note} />
+          <NoteCard key={note.id} note={note} onNoteDelete={onNoteDelete} />
         ))}
       </div>
     </div>
