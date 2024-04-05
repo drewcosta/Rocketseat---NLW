e7f8e40a9faeeb056e32ddc7591db82cbf889c95
Andrew Costa <andrewcosta.contato@gmail.com>
2024-04-05 16:43:58 -0300

    feat: create tables

---
+++ NLW Unite/server/prisma/migrations/20240404174516_add_cascades/migration.sql
@@ -0,0 +1,42 @@
+/*
+  Warnings:
+
+  - You are about to drop the `check-ins` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the column `create_at` on the `attendees` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "check-ins_attendee_id_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "check-ins";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "check_ins" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "attendee_id" INTEGER NOT NULL,
+    CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_attendees" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "event_id" TEXT NOT NULL,
+    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_attendees" ("email", "event_id", "id", "name") SELECT "email", "event_id", "id", "name" FROM "attendees";
+DROP TABLE "attendees";
+ALTER TABLE "new_attendees" RENAME TO "attendees";
+CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
